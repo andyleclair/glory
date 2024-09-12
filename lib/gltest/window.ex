c3e2ad576d571cd602e176387f5b9a8a057d6d9b
Andy LeClair <andyleclair@gmail.com>
2024-09-11 23:46:50 -0400

    part two and 3

--- lib/gltest/window.ex
+++ lib/gltest/window.ex
@@ -13,8 +13,8 @@ defmodule GlTest.Window do
     opts = [size: {800, 600}]
     wx = :wx.new()
     frame = :wxFrame.new(wx, :wx_const.wx_id_any(), ~c"Hello", opts)
-    :wxWindow.connect(frame, :close_window)
 
+    :wxWindow.connect(frame, :close_window)
     :wxFrame.show(frame)
 
     gl_attrib = [
@@ -33,16 +33,23 @@ defmodule GlTest.Window do
     ctx = :wxGLContext.new(canvas)
     :wxGLCanvas.setCurrent(canvas, ctx)
 
-    {shader_program, vao} = init_opengl()
+    {shader_program, vao1, vao2, rect_vao} = init_opengl()
+    frame_counter = :counters.new(1, [:atomics])
 
     send(self(), :update)
+    now = System.monotonic_time(:millisecond)
 
     {frame,
      %{
+       last_time: now,
        frame: frame,
+       frame_counter: frame_counter,
        canvas: canvas,
        shader_program: shader_program,
-       vao: vao
+       fps: 0,
+       vao1: vao1,
+       vao2: vao2,
+       rect_vao: rect_vao
      }}
   end
 
@@ -81,31 +88,62 @@ defmodule GlTest.Window do
     :gl.deleteShader(vertex_shader)
     :gl.deleteShader(fragment_shader)
 
-    vertices =
-      [
-        -0.5,
-        -0.5,
-        0.0,
-        0.5,
-        -0.5,
-        0.0,
-        0.0,
-        0.5,
-        0.0
-      ]
-      |> Enum.reduce(<<>>, fn el, acc -> acc <> <<el::float-native-size(32)>> end)
+    vertices = triangle_vertices()
+    vertices_2 = triangle_vertices_2()
 
-    [vao] = :gl.genVertexArrays(1)
-    [vbo] = :gl.genBuffers(1)
+    [vao1, vao2, rect_vao] = :gl.genVertexArrays(3)
+    [vbo1, vbo2, rect_vbo, ebo] = :gl.genBuffers(4)
 
-    :gl.bindVertexArray(vao)
+    for {vertex_array, vertex_buffer, vertices} <- [
+          {vao1, vbo1, vertices},
+          {vao2, vbo2, vertices_2}
+        ] do
+      :gl.bindVertexArray(vertex_array)
 
-    :gl.bindBuffer(:gl_const.gl_array_buffer(), vbo)
+      :gl.bindBuffer(:gl_const.gl_array_buffer(), vertex_buffer)
+
+      :gl.bufferData(
+        :gl_const.gl_array_buffer(),
+        byte_size(vertices),
+        vertices,
+        :gl_const.gl_static_draw()
+      )
+
+      :gl.vertexAttribPointer(
+        0,
+        3,
+        :gl_const.gl_float(),
+        :gl_const.gl_false(),
+        3 * byte_size(<<0.0::float-size(32)>>),
+        0
+      )
+
+      :gl.enableVertexAttribArray(0)
+
+      :gl.bindBuffer(:gl_const.gl_array_buffer(), 0)
+
+      :gl.bindVertexArray(0)
+    end
+
+    rect_vertices = rectangle_vertices()
+    rect_indices = rectangle_indices()
+
+    :gl.bindVertexArray(rect_vao)
+    :gl.bindBuffer(:gl_const.gl_array_buffer(), rect_vbo)
 
     :gl.bufferData(
       :gl_const.gl_array_buffer(),
-      byte_size(vertices),
-      vertices,
+      byte_size(rect_vertices),
+      rect_vertices,
+      :gl_const.gl_static_draw()
+    )
+
+    :gl.bindBuffer(:gl_const.gl_element_array_buffer(), ebo)
+
+    :gl.bufferData(
+      :gl_const.gl_element_array_buffer(),
+      byte_size(rect_indices),
+      rect_indices,
       :gl_const.gl_static_draw()
     )
 
@@ -119,12 +157,49 @@ defmodule GlTest.Window do
     )
 
     :gl.enableVertexAttribArray(0)
+    {shader_program, vao1, vao2, rect_vao}
+  end
 
-    :gl.bindBuffer(:gl_const.gl_array_buffer(), 0)
+  @triangle_vertices [
+                       [0.0, 1.0, 0.0],
+                       [1.0, 0.0, 0.0],
+                       [1.0, 1.0, 0.0]
+                     ]
+                     |> List.flatten()
+                     |> Enum.reduce(<<>>, fn el, acc -> acc <> <<el::float-native-size(32)>> end)
+  def triangle_vertices do
+    @triangle_vertices
+  end
 
-    :gl.bindVertexArray(0)
+  @triangle_vertices_2 [
+                         [-0.5, -0.5, 0.0],
+                         [0.5, -0.5, 0.0],
+                         [0.0, 0.5, 0.0]
+                       ]
+                       |> List.flatten()
+                       |> Enum.reduce(<<>>, fn el, acc -> acc <> <<el::float-native-size(32)>> end)
+  def triangle_vertices_2 do
+    @triangle_vertices_2
+  end
 
-    {shader_program, vao}
+  @rectangle_vertices [
+                        [0.5, 0.5, 0.0],
+                        [0.5, -0.5, 0.0],
+                        [-0.5, -0.5, 0.0],
+                        [-0.5, 0.5, 0.0]
+                      ]
+                      |> List.flatten()
+                      |> Enum.reduce(<<>>, fn el, acc -> acc <> <<el::float-native-size(32)>> end)
+
+  def rectangle_vertices do
+    @rectangle_vertices
+  end
+
+  @rectangle_indices [[0, 1, 3], [1, 2, 3]]
+                     |> List.flatten()
+                     |> Enum.reduce(<<>>, fn el, acc -> acc <> <<el::native-size(32)>> end)
+  def rectangle_indices do
+    @rectangle_indices
   end
 
   @impl :wx_object
@@ -133,37 +208,67 @@ defmodule GlTest.Window do
   end
 
   @impl :wx_object
-  def handle_info(:stop, %{canvas: canvas} = state) do
+  def handle_info(:stop, %{canvas: canvas, fps_counter_label: fps_counter_label} = state) do
     :wxGLCanvas.destroy(canvas)
+    :wxStaticText.destroy(fps_counter_label)
 
     {:stop, :normal, state}
   end
 
   @impl :wx_object
   def handle_info(:update, state) do
-    render(state)
+    state = render(state)
 
     {:noreply, state}
   end
 
   defp render(%{canvas: canvas} = state) do
-    draw(state)
+    state =
+      state
+      |> update_frame_counter()
+      |> draw()
+
     :wxGLCanvas.swapBuffers(canvas)
     send(self(), :update)
 
-    :ok
+    state
   end
 
-  defp draw(%{canvas: _canvas} = state) do
+  defp draw(%{frame: frame} = state) do
     :gl.clearColor(0.2, 0.1, 0.3, 1.0)
     :gl.clear(:gl_const.gl_color_buffer_bit())
 
     :gl.useProgram(state.shader_program)
 
-    :gl.bindVertexArray(state.vao)
+    :gl.bindVertexArray(state.vao1)
     :gl.drawArrays(:gl_const.gl_triangles(), 0, 3)
 
-    :ok
+    :gl.bindVertexArray(state.vao2)
+    :gl.drawArrays(:gl_const.gl_triangles(), 0, 3)
+
+    :gl.polygonMode(:gl_const.gl_front_and_back(), :gl_const.gl_line())
+    :gl.bindVertexArray(state.rect_vao)
+    :gl.drawElements(:gl_const.gl_triangles(), 6, :gl_const.gl_unsigned_int(), 0)
+    :gl.polygonMode(:gl_const.gl_front_and_back(), :gl_const.gl_fill())
+
+    :wxWindow.setLabel(frame, ~c"FPS: #{state.fps}")
+
+    state
+  end
+
+  def update_frame_counter(%{last_time: last_time, frame_counter: frame_counter} = state) do
+    now = System.monotonic_time(:millisecond)
+    elapsed = now - last_time
+
+    if elapsed > 100 do
+      frames = :counters.get(frame_counter, 1)
+      fps = (frames / elapsed * 1000) |> round()
+      :counters.put(frame_counter, 1, 0)
+      Map.merge(state, %{fps: fps, last_time: now})
+    else
+      :counters.add(frame_counter, 1, 1)
+      state
+    end
   end
 
   def child_spec(opts) do
